d0f812d10ab642def89c80fb16c8dc99b95ab6a4
Robert Portelli <github@robertportelli.com>
2025-01-28 16:47:54 +0000

    Add lib/log_output.bash

---
+++ lib/log_output.bash
@@ -0,0 +1,38 @@
+# Filename: src/log_output.bash
+# Description:
+# Purpose:
+# Options:
+# Usage:
+# Example(s):
+# Requirements:
+# Author:
+#   Robert Portelli
+#   Repository: https://github.com/robert-portelli/readiluks
+# Version:
+#   See repository tags or release notes.
+# License:
+#   See repository license file (e.g., LICENSE.md).
+# Last Updated:
+#   See repository commit history (e.g., `git log`).
+
+# Define log levels
+declare -A LOG_LEVELS=(
+    [DEBUG]=0
+    [INFO]=1
+    [WARNING]=2
+    [ERROR]=3
+)
+
+# Log message function
+lm() {
+    local level=$1
+    local message=$2
+
+    # shellcheck disable=SC2153
+    if (( LOG_LEVELS[$level] >= LOG_LEVELS[$LOG_LEVEL] )); then
+        logger -t "readiluks-$level" "$message"
+        if [[ "$LOG_TO_CONSOLE" == true ]]; then
+            echo "[$(date '+%Y-%m-%d %H:%M:%S')] [$level] $message"
+        fi
+    fi
+}
