ee765c5fd75bb1724ad057eac8a13903ffd551d2
Robert Portelli <github@robertportelli.com>
2025-01-28 20:58:54 +0000

    rename array config to CONFIG

--- src/main.bash
+++ src/main.bash
@@ -15,14 +15,14 @@
 # Last Updated:
 #   See repository commit history (e.g., `git log`).
 
-declare -A config=(
+declare -A CONFIG=(
     [LOG_LEVEL]="INFO"  # Default log level
     [LOG_TO_CONSOLE]=false  # Default: don't log to console
     [BASE_DIR]="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
 )
 
-source "${config[BASE_DIR]}/src/lib/parse_prod_args.bash"
-source "${config[BASE_DIR]}/src/lib/log_prod_output.bash"
+source "${CONFIG[BASE_DIR]}/src/lib/parse_prod_args.bash"
+source "${CONFIG[BASE_DIR]}/src/lib/log_prod_output.bash"
 
 main() {
     parse_arguments "$@"
