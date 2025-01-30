f80f4556dbceed33b413acfad422f8b1ba47b853
Robert Portelli <github@robertportelli.com>
2025-01-30 02:43:45 +0000

    add script that uses act to call the workflow that calls the tests

---
+++ test/run_tests_locally.bash
@@ -0,0 +1,89 @@
+#!/usr/bin/env bash
+
+# Filename:
+# Description:
+# Purpose:
+# Usage:
+# Options:
+# Examples:
+
+# Requirements:
+# Author:
+#   Robert Portelli
+#   Repository: https://github.com/robert-portelli/readiluks
+# Version:
+#   See repository tags or release notes.
+# License:
+#   See repository license file (e.g., LICENSE.md).
+# Last Updated:
+#   See repository commit history (e.g., `git log`).
+
+declare -A CONFIG=(
+    [LOG_LEVEL]="INFO"  # Default log level
+    [LOG_TO_CONSOLE]=false  # Default: don't log to console
+    [BATS_FLAGS]=""
+    [BASE_DIR]="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
+    [IMAGENAME]="robertportelli/test-readiluks:latest"
+    [DOCKERIMAGE]="ubuntu-latest=${config[IMAGENAME]}"
+    [TESTS]=""
+)
+
+source "${CONFIG[BASE_DIR]:?}"/test/lib/parse_test_args.bash
+source "${CONFIG[BASE_DIR]:?}"/test/lib/log_test_output.bash
+
+cleanup() {
+    local containers
+    containers=$(docker ps -a -q --filter ancestor="${CONFIG[IMAGENAME]}")
+
+    if [[ -n "$containers" ]]; then
+        echo "Found containers for '${CONFIG[IMAGENAME]}': $containers"
+        echo "Removing containers..."
+        # shellcheck disable=SC2086
+        docker rm -f $containers || echo "Failed to remove some containers" >&2
+    else
+        echo "No containers found for image '${CONFIG[IMAGENAME]}'."
+    fi
+}
+
+test_common_setup() {
+    gh act workflow_dispatch -j "test-bats-common-setup"
+}
+
+test_parse_prod_args_workflow() {
+    # emulate PR trigger
+    gh act pull_request -j  "test-parser"
+    # emulate manual trigger with default bats flags provided by workflow:
+    gh act workflow_dispatch -j "test-parser"
+    # emulate manual trigger with overriding bats flags:
+    gh act workflow_dispatch -j "test-parser" --input bats-flags="--timing"
+    # emulate manual trigger without bats flags:
+    gh act workflow_dispatch -j "test-parser" --input bats-flags=""
+}
+
+unit_test_parse_prod_args() {
+    gh act workflow_dispatch -j "test-parser"
+    #gh act workflow_dispatch -j "test-parser" --input bats-flags="none"
+}
+
+run_tests() {
+    #test_common_setup
+    #test_parse_prod_args_workflow
+    unit_test_parse_prod_args
+
+}
+
+main() {
+    parse_test_arguments "$@"
+    readonly -A CONFIG
+
+    if [[ "$LOG_LEVEL" == "DEBUG" ]]; then
+        log_config
+    fi
+
+    trap cleanup EXIT
+    run_tests
+}
+
+if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
+    main "$@"
+fi
